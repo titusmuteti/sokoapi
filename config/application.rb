d0492b6b6abeb80739a230e559ebde306eb3a741
Titus Mumo <titusmumo@gmail.com>
2023-11-06 13:39:34 +0300

    configuring routes

--- config/application.rb
+++ config/application.rb
@@ -1,22 +1,34 @@
 require_relative "boot"
 
-require "rails/all"
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+# Add the following line to include the rack-cors gem
+require "rack/cors"
+
 Bundler.require(*Rails.groups)
 
-module Sokoapi
+module MakawascoBackend
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # CORS configuration
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*' # Adjust this to allow requests from specific domains instead of '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+      end
+    end
+
+    config.generators.system_tests = nil
   end
 end
