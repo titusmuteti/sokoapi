bbc11f919e6776cc5f182bf26ceac1213752f1a0
Titus Mumo <titusmumo@gmail.com>
2023-11-06 10:58:35 +0300

    adding fk to addresses

---
+++ db/migrate/20231106075750_add_user_to_addresses.rb
@@ -0,0 +1,5 @@
+class AddUserToAddresses < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :addresses, :user, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_06_074132) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_06_075750) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "addresses", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -56,6 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_06_074132) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "addresses", "users"
   add_foreign_key "orders", "addresses"
   add_foreign_key "orders", "products"
   add_foreign_key "orders", "users"
