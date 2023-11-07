9db1c0789c1ba5b306f86b3123d610125b757104
Titus Mumo <titusmumo@gmail.com>
2023-11-07 09:18:03 +0300

    edting product serializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,5 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id :title, :price, :description, :category, :image, :rating
+  attributes :id, :title, :price, :description, :category, :image, :rating
 
   has_many :orders
   has_many :users, through: :orders
