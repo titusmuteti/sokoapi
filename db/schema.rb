a059b2185f696924014e414c4e147e61950fb71b
Titus Mumo <titusmumo@gmail.com>
2023-11-07 17:46:49 +0300

    adding region and city columns to address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_07_101033) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_07_144455) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_07_101033) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.string "city"
+    t.string "region"
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
