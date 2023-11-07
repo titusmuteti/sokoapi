11a0dc1ca490261515d86598bc8ded4ced4b4179
Titus Mumo <titusmumo@gmail.com>
2023-11-07 11:50:22 +0300

    user CRUD and authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,40 @@
 class UsersController < ApplicationController
+
   def index
     users = User.all
     render json: users, status: :ok
   end
+
+  def show
+    user = User.find_by_id(id: session[:user_id_id])
+      if user
+        render json: user, include: status: :ok
+      else
+        render json: { error: "You must be logged in to access this content" }, status: :unauthorized
+      end
+  end
+
+
+  def create
+    user = User.new(client_params)
+      
+    if user.save
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    orders = Order.where(user_id: user.id)
+
+    if orders.any?
+      orders.each do |Order|
+        Order.update(user_id: nil)
+      end
+    end
 
 
   private
