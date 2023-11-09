9706e4015f2d9aee740ca8921cc9ac9a8e36b8ab
Titus Mumo <titusmumo@gmail.com>
2023-11-09 15:15:00 +0300

    editing controller and serializers

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :phone_number, :address, :password
+  attributes :id, :first_name, :last_name, :phone_number, :password, :email
 
   has_many :addresses
   has_many :orders
