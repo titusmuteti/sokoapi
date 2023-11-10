9cf8f37ddc188aec60ceee0ad1e89983b7149fca
Titus Mumo <titusmumo@gmail.com>
2023-11-10 20:12:21 +0300

    edting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   resources :orders do
-    post 'add_to_cart/:product_id', to: 'orders#add_to_cart', on: :member, as: :add_to_cart
-    patch 'reduce_quantity/:order_item_id', to: 'orders#reduce_quantity', on: :member, as: :reduce_quantity
-    delete 'remove_from_cart/:order_item_id', to: 'orders#remove_from_cart', on: :member, as: :remove_from_cart
+    member do
+      post 'add_to_cart/:product_id', to: 'orders#add_to_cart', as: :add_to_cart
+      patch 'reduce_quantity/:order_item_id', to: 'orders#reduce_quantity', as: :reduce_quantity
+      delete 'remove_from_cart/:order_item_id', to: 'orders#remove_from_cart', as: :remove_from_cart
+    end
   end
 
-  # Add a get route for showing all orders
   get '/orders', to: 'orders#index'
-
-  # Add a show route for displaying a specific order
   get '/orders/:id', to: 'orders#show', as: :show_order
 
   resources :users
