1b4fddc9a3c55813d6b8a78610a8313f7135932d
Titus Mumo <titusmumo@gmail.com>
2023-11-11 12:00:53 +0300

    editing routes and order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,17 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :address
-  has_many :order_details
+  has_many :order_items
 
-  def add_product(product)  
-    existing_order_item = order_items.find_by(product_id: product.id)
-
-    if existing_order_item
-      existing_order_item.quantity += 1
-    else
-      existing_order_item = order_items.build(product: product, quantity: 1, total_price: product.price)
-    end
-
-    save
+  def add_product(product)
+    order_item = order_items.build(product: product, quantity: 1, unit_price: product.price)
+    order_item.save
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,17 +8,5 @@ class User < ApplicationRecord
   
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true
-  
-    def add_product_to_cart(product_id)
-      order = orders.find_or_create_by(order_status: 'cart')
-      product = Product.find(product_id)
-  
-      if order.add_product(product)
-        order
-      else
-        errors.add(:base, 'Failed to add product to cart')
-        nil
-      end
-    end
 end
   
