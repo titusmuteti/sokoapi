5bc5e86fc569a171ae310fcaa9bb5f3a8c802fc3
Titus Mumo <titusmumo@gmail.com>
2023-11-11 14:40:42 +0300

    edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   skip_before_action:verify_authenticity_token
   wrap_parameters format: []
   # protect_from_forgery with: :exception
-  # before_action :authorize
+  before_action :authorize
   before_action :set_current_user
 
 include ActionController::Cookies
