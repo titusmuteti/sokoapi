36485926f3586245ff182d5f991a16a0704c03ed
Titus Mumo <titusmumo@gmail.com>
2023-11-11 17:10:16 +0300

    edit

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Sokoapi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins 'http://localhost:3000', 'https://sokoapi.onrender.com'
-        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
 
