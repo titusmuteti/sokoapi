9a1631b0761be2677e0f8ed3bdc70a004f7c5824
Titus Mumo <titusmumo@gmail.com>
2023-11-30 15:57:09 +0300

    handling quality change

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,4 +3,15 @@ class OrderItem < ApplicationRecord
   belongs_to :order
 
   validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 1 }
+
+  def update_quantity(change_type)
+    case change_type.to_sym
+    when :increase
+      update(quantity: quantity + 1)
+    when :decrease
+      update(quantity: [quantity - 1, 0].max) # Ensure quantity doesn't go below 0
+    else
+      false
+    end
+  end
 end
